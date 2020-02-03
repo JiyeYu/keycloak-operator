7e3f54a1050c75330d456ed5b57a85fb03ed8ad1
JIYE YU <jiye.yu.kb@hitachi.com>
2020-02-03 10:37:48 +0900

    KEYCLOAK-12409:allow image overrides

---
+++ pkg/model/postgresql_common_backup_test.go
@@ -0,0 +1,57 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/keycloak/keycloak-operator/pkg/apis/keycloak/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUtil_Test_GetBackupImage_Without_Override_Image_Set(t *testing.T) {
+	// given
+	keycloak := &v1alpha1.Keycloak{}
+
+	// when
+	returnedImage := GetBackupImage(keycloak)
+
+	// then
+	assert.Equal(t, returnedImage, BackupImage)
+}
+
+func TestUtil_Test_GetBackupImage_With_Override_Image_Set(t *testing.T) {
+	// given
+	keycloak := &v1alpha1.Keycloak{
+		Spec: v1alpha1.KeycloakSpec{
+			ImageOverrides: v1alpha1.KeycloakRelatedImages{
+				Backup: "backup-container:1.0",
+			},
+		},
+	}
+
+	// when
+	returnedImage := GetBackupImage(keycloak)
+
+	// then
+	assert.Equal(t, returnedImage, "backup-container:1.0")
+}
+
+func Test_postgresqlAwsBackupCommonContainers_With_Overrided_Image(t *testing.T) {
+	// given
+	cr := &v1alpha1.KeycloakBackup{}
+	keycloak := &v1alpha1.Keycloak{
+		Spec: v1alpha1.KeycloakSpec{
+			ImageOverrides: v1alpha1.KeycloakRelatedImages{
+				Backup: "backup-container:1.0",
+			},
+		},
+	}
+	currentImage := BackupImage
+
+	// when
+	for _, ele := range postgresqlAwsBackupCommonContainers(cr, keycloak) {
+		currentImage = ele.Image
+	}
+
+	// then
+	assert.Equal(t, currentImage, "backup-container:1.0")
+}
